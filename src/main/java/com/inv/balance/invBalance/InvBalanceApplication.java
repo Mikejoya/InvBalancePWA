8e44e028a4d9c4ad6fac335529cf0f984c5d22ef
Michael Joya López <107166224+Mikejoya@users.noreply.github.com>
2025-09-08 00:32:20 +0000

    Inicializacion del proyecto con spring boot

---
+++ src/main/java/com/inv/balance/invBalance/InvBalanceApplication.java
@@ -0,0 +1,13 @@
+package com.inv.balance.invBalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvBalanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inv/balance/invBalance/InvBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inv.balance.invBalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvBalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
